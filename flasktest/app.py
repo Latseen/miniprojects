614f942fea33d491b9af07d069341e72676ef994
Chet Russell <crusse2015@gmail.com>
2025-02-25 14:16:47 -0800

    Added css stylesheet

--- flasktest/app.py
+++ flasktest/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template("index.html")  # Serves the frontend
+
+#@app.route("/api/message")
+#def api_message():
+#    return jsonify({"message": "Hello from Flask API!"})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0", port=5000, debug=True)
