88350bff01a9737d9c8becd6239f4b66cc99557b
Chet Russell <crusse2015@gmail.com>
2025-02-26 17:14:50 -0800

    Quick update

--- flasktest/app.py
+++ flasktest/app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")  # Serves the frontend
 
-#@app.route("/api/message")
-#def api_message():
-#    return jsonify({"message": "Hello from Flask API!"})
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
